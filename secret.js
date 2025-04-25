0c84dd7a3c0b08f1ee3a34c66e2fa1e048e65480
Dan Barker <danbrakerptbr@gmail.com>
2025-04-24 21:37:16 -0300

    MÙSICAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- secret.js
+++ secret.js
@@ -1,14 +1,17 @@
+let boom = new Audio('./assets/musics/vineboom.mp3');
+
 document.getElementById('zombie-word').addEventListener('click', () => {
     const zombieImage = document.getElementById('zombie-flash');
     zombieImage.style.display = 'block';
     zombieImage.style.opacity = 1;
+    boom.cloneNode(true).play();
   
     setTimeout(() => {
       zombieImage.style.opacity = 0;
-    }, 100); // starts fading after 100ms
+    }, 400); // starts fading after 100ms
   
     setTimeout(() => {
       zombieImage.style.display = 'none';
-    }, 300); // completely gone after 300ms
+    }, 5000); // completely gone after 300ms
   });
   
